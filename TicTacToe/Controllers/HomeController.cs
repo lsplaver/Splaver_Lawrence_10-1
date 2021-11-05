38f367b978e80d62624b9c300c08a873288876f3
Lawrence Splaver <lsplaver@yahoo.com>
2021-11-04 23:21:12 -0400

    Updated Using in HomeController.  Created and partially coded Game.  Partially coded Index for testing purposes.

--- TicTacToe/Controllers/HomeController.cs
+++ TicTacToe/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TicTacToe.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace TicTacToe.Controllers
 {
---
+++ TicTacToe/Models/Game.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Models
+{
+    public class Game
+    {
+        public string Player1 { get; set; }
+        public string Player2 { get; set; }
+        public Dictionary<int, string> CurrentSquareValues { get; set; }
+        public string CurrentPlayer { get; set; }
+        public bool IsGameOver { get; set; }
+
+        public Game()
+        {
+            NewGame();
+        }
+
+        public void NewGame()
+        {
+            for (int x = 1; x < 10; x++)
+            {
+                CurrentSquareValues.Clear();
+                CurrentSquareValues.Add(x, "");
+            }
+            
+            CurrentPlayer = "X";
+            IsGameOver = false;
+            Player1 = "X";
+            Player2 = "O";
+        }
+    }
+}
